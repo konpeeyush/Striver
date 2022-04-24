63035f075a0533f4f5272b31692d6246ea699903
konpeeyush <piyushksingh2589@gmail.com>
2022-04-24 11:23:24 +0530

    Initial Commit, need some improvisation.

---
+++ src/Graphs/Shortest_path_in_Directed_Graph.java
@@ -0,0 +1,8 @@
+package Graphs;
+
+public class Shortest_path_in_Directed_Graph {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Trees/AVL_Trees.java
@@ -0,0 +1,8 @@
+package Trees;
+
+public class AVL_Trees {
+
+    public static void main(String[] args) {
+
+    }
+}
